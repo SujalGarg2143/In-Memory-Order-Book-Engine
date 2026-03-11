ed01a1b0d244f408c66ac0a67d039226f5a323b8
SujalGarg2143 <sujal2143.be23@chitkara.edu.in>
2026-03-11 16:51:38 +0530

    Implemented order.py

---
+++ order.py
@@ -0,0 +1,8 @@
+class Order:
+
+    def __init__(self, order_id, side, price, quantity, timestamp):
+        self.id = order_id
+        self.side = side
+        self.price = price
+        self.quantity = quantity
+        self.timestamp = timestamp
